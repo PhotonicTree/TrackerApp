c3333c7e2f42c54a359b09153fffa1f5cefdaec8
KonradTkaczyk <k.tkaczyk@mnemosis.pl>
2022-07-03 20:44:12 +0200

    Add imageViewer and frameProvider to enginge root context

--- TrackerApp/main.cpp
+++ TrackerApp/main.cpp
@@ -2,10 +2,12 @@
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 #include <QQuickImageProvider>
+#include <iostream>
 
 #include "BackendController.h"
 #include "BackendWorker.h"
 #include "FrameImageProvider.h"
+#include "ImageViewer.h"
 
 int main(int argc, char *argv[])
 {
@@ -20,13 +22,16 @@ int main(int argc, char *argv[])
     BackendWorker* backendWorker(new BackendWorker);
     FrameImageProvider* frameImageProvider(new FrameImageProvider);
     auto backendController = BackendController(backendWorker);
+    ImageViewer* imageViewer(new ImageViewer);
     engine.rootContext()->setContextProperty("backendController", &backendController);
-    engine.rootContext()->setContextProperty("frameImageProvieder", frameImageProvider);
-
+    engine.rootContext()->setContextProperty("frameImageProvider", frameImageProvider);
+    engine.rootContext()->setContextProperty("imageViewer", imageViewer);
+    engine.addImageProvider("frameProvider", frameImageProvider);
     
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
     if (engine.rootObjects().isEmpty())
         return -1;
 
+    QObject::connect(imageViewer, &ImageViewer::newImage, frameImageProvider, &FrameImageProvider::updateImage);
     return app.exec();
 }

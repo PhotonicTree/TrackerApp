771d391b0eef18789899a9c90bd64f5ac8c06d42
KonradTkaczyk <k.tkaczyk@mnemosis.pl>
2022-07-24 16:18:31 +0200

    Add simpleBlobDetector Parameters

--- TrackerApp/BackendWorker.cpp
+++ TrackerApp/BackendWorker.cpp
@@ -32,4 +32,47 @@ Q_INVOKABLE void BackendWorker::DisplaySequence()
 Q_INVOKABLE cv::VideoCapture BackendWorker::GetCapture()
 {
     return videoCapture;
+
+cv::SimpleBlobDetector::Params InitializeSimpleBlobDetectorParmeters()
+{
+    cv::SimpleBlobDetector::Params circleBlobDetectorParams;
+
+    // The following parameters are described here:
+    // https://docs.opencv.org/4.5.1/d0/d7a/classcv_1_1SimpleBlobDetector.html
+
+    // Filter by intensity (thresholding)
+    circleBlobDetectorParams.thresholdStep = 30;
+    circleBlobDetectorParams.minThreshold = 50;
+    circleBlobDetectorParams.maxThreshold = 231;
+    circleBlobDetectorParams.minRepeatability = 2;
+    circleBlobDetectorParams.minDistBetweenBlobs = 20;
+
+    // Filter by color
+    circleBlobDetectorParams.filterByColor = true;
+    circleBlobDetectorParams.blobColor = 0;
+
+    // Filter by area
+    circleBlobDetectorParams.filterByArea = true;
+    circleBlobDetectorParams.minArea = 80;
+    circleBlobDetectorParams.maxArea = 1200;
+
+    // Filter by circularity
+    circleBlobDetectorParams.filterByCircularity = true;
+    circleBlobDetectorParams.minCircularity = 0.40F;
+    circleBlobDetectorParams.maxCircularity = 0.94F;
+
+    // Filter by convexity
+    circleBlobDetectorParams.filterByConvexity = true;
+    circleBlobDetectorParams.minConvexity = 0.93F;
+    circleBlobDetectorParams.maxConvexity = 1.01F;
+
+    // Filter by inertia
+    // https://stackoverflow.com/questions/14770756/opencv-simpleblobdetector-filterbyinertia-meaning
+    circleBlobDetectorParams.filterByInertia = true;
+    circleBlobDetectorParams.minInertiaRatio = 0.25F;
+    circleBlobDetectorParams.maxInertiaRatio = 1.01F;
+
+    return circleBlobDetectorParams;
+}
+
 }

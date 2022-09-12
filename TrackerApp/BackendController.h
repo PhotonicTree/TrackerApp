225855a0668e16a580aa2cfd878c9eb695119f1d
KonradTkaczyk <k.tkaczyk@mnemosis.pl>
2022-09-12 20:06:04 +0200

    Clean BackendController.h and  add documentation

--- TrackerApp/BackendController.h
+++ TrackerApp/BackendController.h
@@ -10,35 +10,26 @@ class BackendController : public QObject
 {
 	Q_OBJECT
 public:
-	/*! BackendController constructor.
-		\param[in] backendWorker Backend worker.
+	/*BackendController constructor.
+		\param[in] backendWorker Backend worker class.
 		\param[in] parent Parent QObject.
 	*/
 	BackendController(BackendWorker* backendWorker, QObject* parent = nullptr);
 
-	//! Destructor.
+	//Destructor.
 	~BackendController();
 
 signals:
-	//! Signal used to emit to start importing action.
+	//Signal used to emit to start importing action.
 	void startImporting(const QString& path);
 
+	//Signal used to emit to start displaying action.
 	void startDisplaying();
 
+	//Signal emitted when sequence import is finished.
 	void importingFinished();
 
 private:
-	BackendWorker* backendWorker; //!< A pointer to backend Worker, which is created in main file and do all backend methods.
-	QThread backendThread;		  //!< QThread for heavy operations, which runs simultaneously and is called from QML by connects.
-
-private:
-	cv::Mat image;												//!< Image to display.
-	cv::VideoCapture cap;
-	//QTimer tUpdate;
-
-signals:
-	void newImage(QImage&);
-
-public:
-	//void streamVideo();
+	BackendWorker* backendWorker; //!< A pointer to Backend Worker, which is created in main file and do all the backend methods.
+	QThread backendThread;		  //!< QThread for heavy operations, which runs simultaneously and is called from QML by connections.
 };

72ed821e9c2685e0190ab61d2e1bb6a75aeb38a2
KonradTkaczyk <k.tkaczyk@mnemosis.pl>
2022-09-12 20:07:55 +0200

    Clean  BackendWorker.cpp file, move tracking method to Trackers.cpp

--- TrackerApp/BackendWorker.cpp
+++ TrackerApp/BackendWorker.cpp
@@ -6,7 +6,7 @@ Q_INVOKABLE void BackendWorker::ImportSequence(const QString& path)
 	videoCapture.open(sequencePath);
 
     if (!videoCapture.isOpened()) {
-        std::cout << "Error opening video stream or file" << std::endl;
+        std::cout << "Error opening video stream or file!" << std::endl;
         return;
     }
 
@@ -31,10 +31,10 @@ Q_INVOKABLE void BackendWorker::DisplaySequence()
     imageViewer->TriggerStreamingSequence(videoCapture);
 }
 
-Q_INVOKABLE void BackendWorker::GetFirstFrameROI()
+Q_INVOKABLE void BackendWorker::GetFirstFrameROIs()
 {
     auto& firstFrame = sequence.front();
-    ROI = cv::selectROI("tracker", firstFrame);
+    cv::selectROIs("tracker", firstFrame, ROIs);
     std::cout << "Roi selected";
 }
 
@@ -69,191 +69,5 @@ Q_INVOKABLE void BackendWorker::RunAllTrackers()
                 //    break;
             }
         }
-    }
-    // RunTrackeDaSiamRPN();
-}
-
-void BackendWorker::HoughCirclesTracker()
-{
-    for (const auto& frame : sequence)
-    {
-        std::vector<cv::Vec3f> circles;
-        cv::Mat grayImage;
-        cv::Mat canny;
-        auto ROIImage = frame(ROI);
-        cv::cvtColor(ROIImage, grayImage, cv::COLOR_BGR2GRAY);
-        cv::medianBlur(grayImage, grayImage, 5);
-       // cv::Canny(grayImage, canny, 160, 0, 3);
-        cv::HoughCircles(grayImage, circles, cv::HOUGH_GRADIENT, 1, grayImage.rows / 16, 140, 10, 1, 10);
-
-        for (size_t i = 0; i < circles.size(); i++)
-        {
-            cv::Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
-            int radius = cvRound(circles[i][2]);
-            // draw the circle center
-            cv::circle(ROIImage, center, 3, cv::Scalar(0, 255, 0), -1, 8, 0);
-            // draw the circle outline
-            circle(ROIImage, center, radius, cv::Scalar(0, 0, 255), 3, 8, 0);
-        }
-        cv::namedWindow("circles", 1);
-        cv::imshow("circles", ROIImage);
-        cv::waitKey(0);
-    }
-}
-
-cv::SimpleBlobDetector::Params InitializeSimpleBlobDetectorParmeters()
-{
-    cv::SimpleBlobDetector::Params circleBlobDetectorParams;
-
-    // The following parameters are described here:
-    // https://docs.opencv.org/4.5.1/d0/d7a/classcv_1_1SimpleBlobDetector.html
-
-    // Filter by intensity (thresholding)
-    circleBlobDetectorParams.thresholdStep = 30;
-    circleBlobDetectorParams.minThreshold = 50;
-    circleBlobDetectorParams.maxThreshold = 231;
-    circleBlobDetectorParams.minRepeatability = 2;
-    circleBlobDetectorParams.minDistBetweenBlobs = 20;
-
-    // Filter by color
-    circleBlobDetectorParams.filterByColor = true;
-    circleBlobDetectorParams.blobColor = 0;
-
-    // Filter by area
-    circleBlobDetectorParams.filterByArea = true;
-    circleBlobDetectorParams.minArea = 80;
-    circleBlobDetectorParams.maxArea = 1200;
-
-    // Filter by circularity
-    circleBlobDetectorParams.filterByCircularity = true;
-    circleBlobDetectorParams.minCircularity = 0.40F;
-    circleBlobDetectorParams.maxCircularity = 0.94F;
-
-    // Filter by convexity
-    circleBlobDetectorParams.filterByConvexity = true;
-    circleBlobDetectorParams.minConvexity = 0.93F;
-    circleBlobDetectorParams.maxConvexity = 1.01F;
-
-    // Filter by inertia
-    // https://stackoverflow.com/questions/14770756/opencv-simpleblobdetector-filterbyinertia-meaning
-    circleBlobDetectorParams.filterByInertia = true;
-    circleBlobDetectorParams.minInertiaRatio = 0.25F;
-    circleBlobDetectorParams.maxInertiaRatio = 1.01F;
-
-    return circleBlobDetectorParams;
-}
-
-void BackendWorker::DetectMarkersOnImage()
-{
-    auto firstFrame = sequence.front();
-    auto blobDetector = cv::SimpleBlobDetector::create(InitializeSimpleBlobDetectorParmeters());
-    std::vector<cv::KeyPoint> keypoints;
-    blobDetector->detect(firstFrame, keypoints);
-
-    for (size_t i = 0; i < keypoints.size(); i++)
-    {
-        cv::Point center(cvRound(keypoints[i].pt.x), cvRound(keypoints[i].pt.y));
-        int radius = 10;
-        // draw the circle center
-        cv::circle(firstFrame, center, 3, cv::Scalar(0, 255, 0), -1, 8, 0);
-        // draw the circle outline
-        circle(firstFrame, center, radius, cv::Scalar(0, 0, 255), 3, 8, 0);
-    }
-}
-
-void BackendWorker::RunTrackerCSRT()
-{
-    GetFirstFrameROI();
-    cv::Ptr<cv::Tracker> tracker = cv::TrackerCSRT::create();
-    // initialize the tracker
-
-    tracker->init(sequence.front(), ROI);
-    // perform the tracking process
-    printf("Start the tracking process, press ESC to quit.\n");
-    for (auto& frame : sequence) {
-        // stop the program if no more images
-        if (frame.rows == 0 || frame.cols == 0)
-            break;
-        // update the tracking result
-        tracker->update(frame, ROI);
-        // draw the tracked object
-        rectangle(frame, ROI, cv::Scalar(255, 0, 0), 2, 1);
-        // show image with the tracked object
-        imshow("tracker", frame);
-        //quit on ESC button
-        if (cv::waitKey(1) == 27)break;
-    }
-}
-
-void BackendWorker::RunTrackerDaSiamRPN()
-{
-    GetFirstFrameROI();
-    cv::Ptr<cv::Tracker> tracker = cv::TrackerDaSiamRPN::create();
-    // initialize the tracker
-
-    tracker->init(sequence.front(), ROI);
-    // perform the tracking process
-    printf("Start the tracking process, press ESC to quit.\n");
-    for (auto& frame : sequence) {
-        // stop the program if no more images
-        if (frame.rows == 0 || frame.cols == 0)
-            break;
-        // update the tracking result
-        tracker->update(frame, ROI);
-        // draw the tracked object
-        rectangle(frame, ROI, cv::Scalar(255, 0, 0), 2, 1);
-        // show image with the tracked object
-        imshow("tracker", frame);
-        //quit on ESC button
-        if (cv::waitKey(1) == 27)break;
-    }
-}
-
-void BackendWorker::RunTrackerGOTURN()
-{
-    GetFirstFrameROI();
-    cv::Ptr<cv::Tracker> tracker = cv::TrackerGOTURN::create();
-    // initialize the tracker
-
-    tracker->init(sequence.front(), ROI);
-    // perform the tracking process
-    printf("Start the tracking process, press ESC to quit.\n");
-    for (auto& frame : sequence) {
-        // stop the program if no more images
-        if (frame.rows == 0 || frame.cols == 0)
-            break;
-        // update the tracking result
-        tracker->update(frame, ROI);
-        // draw the tracked object
-        rectangle(frame, ROI, cv::Scalar(255, 0, 0), 2, 1);
-        // show image with the tracked object
-        imshow("tracker", frame);
-        //quit on ESC button
-        if (cv::waitKey(1) == 27)break;
-    }
-}
-
-void BackendWorker::RunTrackerMOSSE()
-{
-    GetFirstFrameROI();
-    auto tracker = cv::legacy::TrackerMOSSE::create();
-    // initialize the tracker
-
-    tracker->init(sequence.front(), ROI);
-    // perform the tracking process
-    printf("Start the tracking process, press ESC to quit.\n");
-    for (auto& frame : sequence) {
-        // stop the program if no more images
-        if (frame.rows == 0 || frame.cols == 0)
-            break;
-        // update the tracking result
-        auto ROI2d = static_cast<cv::Rect2d>(ROI);
-        tracker->update(frame, ROI2d);
-        // draw the tracked object
-        rectangle(frame, ROI, cv::Scalar(255, 0, 0), 2, 1);
-        // show image with the tracked object
-        imshow("tracker", frame);
-        //quit on ESC button
-        if (cv::waitKey(1) == 27)break;
     }
 }

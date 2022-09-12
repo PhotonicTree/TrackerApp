d3870ace0fbfaa545820fb3a36b07cfda86c7d8d
KonradTkaczyk <k.tkaczyk@mnemosis.pl>
2022-09-12 20:16:33 +0200

    Clean Trackers.h file and add documentation

--- TrackerApp/Trackers.h
+++ TrackerApp/Trackers.h
@@ -8,48 +8,19 @@
 #include <opencv2/core/utility.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
-#include <opencv2/core/cvstd.hpp>
 
-
-inline cv::Ptr<cv::Tracker> createTrackerByName(cv::String name)
-{
-	cv::Ptr<cv::Tracker> tracker;
-	//cv::Ptr<cv::legacy::Tracker> tracker;
-
-	if (name == "KCF")
-		tracker = cv::TrackerKCF::create();
-	//else if (name == "TLD")
-	//{
-	//	auto legacyTracker = cv::TrackerKCF::create();
-	//	tracker = cv::Ptr<cv::Tracker>::dynamicCast(legacyTracker);
-	//}
-	else if (name == "BOOSTING")
-		//tracker = cv::legacy::TrackerBoosting::create();
-	//else if (name == "MEDIAN_FLOW")
-		//tracker = cv::legacy::TrackerMedianFlow::create();
-	//else if (name == "MIL")
-		tracker = cv::TrackerMIL::create();
-	else if (name == "GOTURN")
-		tracker = cv::TrackerGOTURN::create();
-	//else if (name == "MOSSE")
-		//tracker = cv::legacy::TrackerMOSSE::create();
-	else
-		CV_Error(cv::Error::StsBadArg, "Invalid tracking algorithm name\n");
-
-	return tracker;
-}
-
-class BaseTracker : public cv::legacy::MultiTracker
+class BaseTracker
 {
 public:
-	//BaseTracker();
-	//BaseTracker(cv::Mat& image, std::vector<cv::Rect>& boundingBoxes, std::vector<cv::Mat>& images);
-	//~BaseTracker();
+	BaseTracker();
+	~BaseTracker();
 
+	/*Pure virtual function dedicated to inheriting classes for performing tracking.*/
 	virtual void RunTracking() = 0;
-	std::vector<cv::Rect> ROIs;
-	cv::Mat firstFrame;
-	std::vector<cv::Mat> sequence;
+
+	std::vector<cv::Rect> ROIs;			//Vector of cv::Rect with selected regions of interests by user.
+	std::vector<cv::Mat> sequence;		//Vector with sequence of images to be analyze.
+};
 
 class MultiBlobDetectorTracker : public BaseTracker
 {
@@ -69,10 +40,16 @@ private:
 	int circleRadius;
 };
 
-class MultiTrackerMIL : public BaseTracker
+class MultiTrackerMIL : public BaseTracker, public cv::legacy::MultiTracker
 {
 public:
-	void InitializeTracker(cv::Mat& image, std::vector<cv::Rect>& boundingBoxes, std::vector<cv::Mat>& images);
+	/*Method used to initialize the tracker.
+		\param[in] boundingBoxes std::vector<cv::Rect> with selected bounding boxes.
+		\param[in] images std::vector<cv::Mat> with loaded sequence of images.
+	*/
+	void InitializeTracker(std::vector<cv::Rect>& boundingBoxes, std::vector<cv::Mat>& images);
+
+	/*Method used to run tracking of markers on given sequence and ROIs.*/
 	virtual void RunTracking();
 };
 
@@ -89,51 +66,106 @@ public:
 	virtual void RunTracking();
 };
 
-class MultiTrackerCSRT : public BaseTracker
+class MultiTrackerMOSSE : public BaseTracker, public cv::legacy::MultiTracker
 {
 public:
-	void InitializeTracker(cv::Mat& image, std::vector<cv::Rect>& boundingBoxes, std::vector<cv::Mat>& images);
+	/*Method used to initialize the tracker.
+		\param[in] boundingBoxes std::vector<cv::Rect> with selected bounding boxes.
+		\param[in] images std::vector<cv::Mat> with loaded sequence of images.
+	*/
+	void InitializeTracker(std::vector<cv::Rect>& boundingBoxes, std::vector<cv::Mat>& images);
+
+	/*Method used to run tracking of markers on given sequence and ROIs.*/
 	virtual void RunTracking();
 };
 
-class MultiTrackerGOTURN : public BaseTracker
+class MultiTrackerCSRT : public BaseTracker, public cv::legacy::MultiTracker
 {
 public:
-	void InitializeTracker(cv::Mat& image, std::vector<cv::Rect>& boundingBoxes, std::vector<cv::Mat>& images);
+	/*Method used to initialize the tracker.
+		\param[in] boundingBoxes std::vector<cv::Rect> with selected bounding boxes.
+		\param[in] images std::vector<cv::Mat> with loaded sequence of images.
+	*/
+	void InitializeTracker(std::vector<cv::Rect>& boundingBoxes, std::vector<cv::Mat>& images);
+
+	/*Method used to run tracking of markers on given sequence and ROIs.*/
 	virtual void RunTracking();
 };
 
-class MultiTrackerDaSiamRPN : public BaseTracker
+class MultiTrackerGOTURN : public BaseTracker, public cv::legacy::MultiTracker
 {
 public:
-	void InitializeTracker(cv::Mat& image, std::vector<cv::Rect>& boundingBoxes, std::vector<cv::Mat>& images);
+	/*Method used to initialize the tracker.
+		\param[in] boundingBoxes std::vector<cv::Rect> with selected bounding boxes.
+		\param[in] images std::vector<cv::Mat> with loaded sequence of images.
+	*/
+	void InitializeTracker(std::vector<cv::Rect>& boundingBoxes, std::vector<cv::Mat>& images);
+
+	/*Method used to run tracking of markers on given sequence and ROIs.*/
 	virtual void RunTracking();
 };
 
-class MultiTrackerTLD : public BaseTracker
+class MultiTrackerDaSiamRPN : public BaseTracker, public cv::legacy::MultiTracker
 {
 public:
-	void InitializeTracker(cv::Mat& image, std::vector<cv::Rect>& boundingBoxes, std::vector<cv::Mat>& images);
+	/*Method used to initialize the tracker.
+		\param[in] boundingBoxes std::vector<cv::Rect> with selected bounding boxes.
+		\param[in] images std::vector<cv::Mat> with loaded sequence of images.
+	*/
+	void InitializeTracker(std::vector<cv::Rect>& boundingBoxes, std::vector<cv::Mat>& images);
+
+	/*Method used to run tracking of markers on given sequence and ROIs.*/
 	virtual void RunTracking();
 };
 
-class MultiTrackerBoosting : public BaseTracker
+class MultiTrackerTLD : public BaseTracker, public cv::legacy::MultiTracker
 {
 public:
-	void InitializeTracker(cv::Mat& image, std::vector<cv::Rect>& boundingBoxes, std::vector<cv::Mat>& images);
+	/*Method used to initialize the tracker.
+		\param[in] boundingBoxes std::vector<cv::Rect> with selected bounding boxes.
+		\param[in] images std::vector<cv::Mat> with loaded sequence of images.
+	*/
+	void InitializeTracker(std::vector<cv::Rect>& boundingBoxes, std::vector<cv::Mat>& images);
+
+	/*Method used to run tracking of markers on given sequence and ROIs.*/
 	virtual void RunTracking();
 };
 
-class MultiTrackerKCF : public BaseTracker
+class MultiTrackerBoosting : public BaseTracker, public cv::legacy::MultiTracker
 {
 public:
-	void InitializeTracker(cv::Mat& image, std::vector<cv::Rect>& boundingBoxes, std::vector<cv::Mat>& images);
+	/*Method used to initialize the tracker.
+		\param[in] boundingBoxes std::vector<cv::Rect> with selected bounding boxes.
+		\param[in] images std::vector<cv::Mat> with loaded sequence of images.
+	*/
+	void InitializeTracker(std::vector<cv::Rect>& boundingBoxes, std::vector<cv::Mat>& images);
+
+	/*Method used to run tracking of markers on given sequence and ROIs.*/
 	virtual void RunTracking();
 };
 
-class MultiTrackerMedianFlow : public BaseTracker
+class MultiTrackerKCF : public BaseTracker, public cv::legacy::MultiTracker
 {
 public:
-	void InitializeTracker(cv::Mat& image, std::vector<cv::Rect>& boundingBoxes, std::vector<cv::Mat>& images);
+	/*Method used to initialize the tracker.
+		\param[in] boundingBoxes std::vector<cv::Rect> with selected bounding boxes.
+		\param[in] images std::vector<cv::Mat> with loaded sequence of images.
+	*/
+	void InitializeTracker(std::vector<cv::Rect>& boundingBoxes, std::vector<cv::Mat>& images);
+
+	/*Method used to run tracking of markers on given sequence and ROIs.*/
+	virtual void RunTracking();
+};
+
+class MultiTrackerMedianFlow : public BaseTracker, public cv::legacy::MultiTracker
+{
+public:
+	/*Method used to initialize the tracker.
+		\param[in] boundingBoxes std::vector<cv::Rect> with selected bounding boxes.
+		\param[in] images std::vector<cv::Mat> with loaded sequence of images.
+	*/
+	void InitializeTracker(std::vector<cv::Rect>& boundingBoxes, std::vector<cv::Mat>& images);
+
+	/*Method used to run tracking of markers on given sequence and ROIs.*/
 	virtual void RunTracking();
 };

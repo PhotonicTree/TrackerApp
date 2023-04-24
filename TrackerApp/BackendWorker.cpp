46ac897d4b52d062757208748c864435070bda69
KonradTkaczyk <k.tkaczyk@mnemosis.pl>
2023-04-24 12:56:23 +0200

    Add JsonTrackerObject to BackendWorker and save results in json in BackendWorker

--- TrackerApp/BackendWorker.cpp
+++ TrackerApp/BackendWorker.cpp
@@ -1,7 +1,11 @@
 #include "BackendWorker.h"
+#include "JsonTrackerObject.h"
+
+#include <fstream>
 
 Q_INVOKABLE void BackendWorker::ImportSequence(const QString& path)
 {
+    sequence.clear();
 	const auto sequencePath = path.toStdString();
 	videoCapture.open(sequencePath);
     sequenceName = sequencePath.substr(sequencePath.find_last_of("/\\") + 1);
@@ -51,6 +55,9 @@ Q_INVOKABLE void BackendWorker::GetSelectedTrackers(std::vector<bool> selectedTr
 Q_INVOKABLE void BackendWorker::RunAllTrackers()
 {
     GetFirstFrameROIs();
+    JsonTrackerObject jsonTrackersObject;
+    auto outputFileName = sequenceName.substr(0, sequenceName.find_first_of('.'));
+    outputFileName += ".json";
     for (const auto selectedTracker : selectedTrackers)
     {
         // Check if current switch is pressed.
@@ -59,65 +66,69 @@ Q_INVOKABLE void BackendWorker::RunAllTrackers()
             switch (selectedTracker.first) {
             case BackendWorkerHelpers::TrackerType::CSRT: {
                 MultiTrackerCSRT* tracker(new MultiTrackerCSRT);
-                tracker->InitializeTracker(ROIs, sequence, sequenceName);
-                tracker->RunTracking();
+                tracker->InitializeTracker(ROIs, sequence);
+                tracker->RunTracking(jsonTrackersObject);
             }
                                                         break;
             case BackendWorkerHelpers::TrackerType::MOSSE: {
                 MultiTrackerMOSSE* tracker(new MultiTrackerMOSSE);
-                tracker->InitializeTracker(ROIs, sequence, sequenceName);
-                tracker->RunTracking();
+                tracker->InitializeTracker(ROIs, sequence);
+                tracker->RunTracking(jsonTrackersObject);
             }
                                                          break;
             /*case BackendWorkerHelpers::TrackerType::DaSiamRPN: {
                 MultiTrackerMIL* tracker(new MultiTrackerMIL);
-                tracker->InitializeTracker(ROIs, sequence, sequenceName);
+                tracker->InitializeTracker(ROIs, sequence);
                 tracker->RunTracking();
             }
                 break; */
                 /*case BackendWorkerHelpers::TrackerType::GOTURN: {
                     MultiTrackerMIL* tracker(new MultiTrackerMIL);
-                    tracker->InitializeTracker(ROIs, sequence, sequenceName);
+                    tracker->InitializeTracker(ROIs, sequence);
                     tracker->RunTracking();
                 }
                     break; */
             case BackendWorkerHelpers::TrackerType::MIL: {
                 MultiTrackerMIL* tracker(new MultiTrackerMIL);
-                tracker->InitializeTracker(ROIs, sequence, sequenceName);
-                tracker->RunTracking();
+                tracker->InitializeTracker(ROIs, sequence);
+                tracker->RunTracking(jsonTrackersObject);
             }
                                                        break;
             case BackendWorkerHelpers::TrackerType::MedianFlow: {
                 MultiTrackerMedianFlow* tracker(new MultiTrackerMedianFlow);
-                tracker->InitializeTracker(ROIs, sequence, sequenceName);
-                tracker->RunTracking();
+                tracker->InitializeTracker(ROIs, sequence);
+                tracker->RunTracking(jsonTrackersObject);
             }
                                                               break;
             case BackendWorkerHelpers::TrackerType::KCF: {
                 MultiTrackerKCF* tracker(new MultiTrackerKCF);
-                tracker->InitializeTracker(ROIs, sequence, sequenceName);
-                tracker->RunTracking();
+                tracker->InitializeTracker(ROIs, sequence);
+                tracker->RunTracking(jsonTrackersObject);
             }
                                                        break;
             case BackendWorkerHelpers::TrackerType::Boosting: {
                 MultiTrackerBoosting* tracker(new MultiTrackerBoosting);
-                tracker->InitializeTracker(ROIs, sequence, sequenceName);
-                tracker->RunTracking();
+                tracker->InitializeTracker(ROIs, sequence);
+                tracker->RunTracking(jsonTrackersObject);
             }
                                                             break;
             case BackendWorkerHelpers::TrackerType::HoughCircles: {
                 MultiHoughCirclesTracker* tracker(new MultiHoughCirclesTracker);
-                tracker->InitializeTracker(ROIs, sequence, sequenceName);
-                tracker->RunTracking();
+                tracker->InitializeTracker(ROIs, sequence);
+                tracker->RunTracking(jsonTrackersObject);
             }
                                                                 break;
             case BackendWorkerHelpers::TrackerType::BlobDetector: {
                 MultiBlobDetectorTracker* tracker(new MultiBlobDetectorTracker);
-                tracker->InitializeTracker(ROIs, sequence, sequenceName);
-                tracker->RunTracking();
+                tracker->InitializeTracker(ROIs, sequence);
+                tracker->RunTracking(jsonTrackersObject);
             }
                                                                 break;
-            }
+            } 
         }
     }
+    std::ofstream exportJSONFile("Results\\" + outputFileName);
+    exportJSONFile << jsonTrackersObject.dump(4);
+    exportJSONFile.close();
+    std::cout << "File exported";
 }
